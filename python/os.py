38a3f127a54a82c2d1d26e07a1c16e031fb7f1a8
Krystian Hebel <krystian.hebel@3mdeb.com>
2018-09-05 17:14:36 +0200

    python/os.py: add `walk` function
    Signed-off-by: Krystian Hebel <krystian.hebel@3mdeb.com>

--- python/os.py
+++ python/os.py
@@ -386,5 +386,39 @@ unsetenv = _bits._unsetenv
 # wait3
 # wait4
 # waitpid
-# walk
+
+def walk(top, topdown=True, onerror=None, followlinks=False):
+    islink, join, isdir = path.islink, path.join, path.isdir
+
+    # We may not have read permission for top, in which case we can't
+    # get a list of the files the directory contains.  os.path.walk
+    # always suppressed the exception then, rather than blow up for a
+    # minor reason when (say) a thousand readable directories are still
+    # left to visit.  That logic is copied here.
+    try:
+        # Note that listdir and error are globals in this module due
+        # to earlier import-*.
+        names = listdir(top)
+    except error, err:
+        if onerror is not None:
+            onerror(err)
+        return
+
+    dirs, nondirs = [], []
+    for name in names:
+        if isdir(join(top, name)):
+            dirs.append(name)
+        else:
+            nondirs.append(name)
+
+    if topdown:
+        yield top, dirs, nondirs
+    for name in dirs:
+        new_path = join(top, name)
+        if followlinks or not islink(new_path):
+            for x in walk(new_path, topdown, onerror, followlinks):
+                yield x
+    if not topdown:
+        yield top, dirs, nondirs
+
 # write
